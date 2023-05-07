d1352b22904007abd555122c234eb50656ad1eff
SecondoOne <131069060+SecondoOne@users.noreply.github.com>
2023-05-07 16:18:53 +0200

    Add files via upload

--- src/main/java/Exception/DataException.java
+++ src/main/java/Exception/DataException.java
@@ -1,12 +1,21 @@
 package Exception;
 
 
-public class DataException extends Exception{
+import View.Utility.ExceptionDisplay;
 
-    public DataException() {}
+public class DataException extends Exception{
+    private String message;
+
+    public DataException() {
+        this.message = getMessage();
+        ExceptionDisplay display = new ExceptionDisplay(message);
+    }
+
 
     @Override
     public String getMessage() {
         return "Erreur, la base de donnée n'est pas connectée ou est vide";
     }
+
+
 }
