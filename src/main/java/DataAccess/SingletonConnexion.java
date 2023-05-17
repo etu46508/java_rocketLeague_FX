18363beb18531e663aa7f661c0dc71809d4959e1
SecondoOne <131069060+SecondoOne@users.noreply.github.com>
2023-05-17 17:07:45 +0200

    CRUD 150% + Patterns 150% + thread 150% + research 100% + Exception 100% + Clean code 99% + Tache métier 100% + Test unitaire ?%

--- src/main/java/DataAccess/SingletonConnexion.java
+++ src/main/java/DataAccess/SingletonConnexion.java
@@ -3,14 +3,15 @@ package DataAccess;
 import java.sql.*;
 
 import Exception.DataException;
+
 public class SingletonConnexion   {
     private static Connection singletonConnexion;
 
     private SingletonConnexion() throws DataException {
         try {
             //singletonConnexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/rocketleague", "root","SecondoOne21");
-            //singletonConnexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/rocketleague", "root","Boulettedu13");
-            singletonConnexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/rocketleague", "root","B@tterie!147");
+            singletonConnexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/rocketleague", "root","Boulettedu13");
+            //singletonConnexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/rocketleague", "root","B@tterie!147");
         } catch (SQLException exception) {
             throw new DataException();
         }
--- src/main/java/View/WelcomePage.java
+++ src/main/java/View/WelcomePage.java
@@ -20,8 +20,8 @@ public class WelcomePage {
             BorderPane root = new BorderPane();
 
             //Image picture = new Image("C:\\Users\\Léonard\\Documents\\GitHub\\java_rocketLeague_FX\\src\\images\\logoRocketLeagueBlanc.png");
-            //Image picture = new Image("C:\\Users\\Robin\\OneDrive\\Documents\\GitHub\\java_rocketLeague_FX\\src\\images\\logoRocketLeagueBlanc.png");
-            Image picture = new Image("C:\\Users\\merlin\\Desktop\\iesn\\bloc 2\\java\\ProjetJavaFxV0\\ProjetJavaFxV0\\src\\images\\logoRocketLeagueBlanc.png");
+            Image picture = new Image("C:\\Users\\Robin\\OneDrive\\Documents\\GitHub\\java_rocketLeague_FX\\src\\images\\logoRocketLeagueBlanc.png");
+            //Image picture = new Image("C:\\Users\\merlin\\Desktop\\iesn\\bloc 2\\java\\ProjetJavaFxV0\\ProjetJavaFxV0\\src\\images\\logoRocketLeagueBlanc.png");
 
             Rectangle pictureContent = new Rectangle(400,200);
             BorderPane nordPane = new BorderPane();
@@ -31,8 +31,9 @@ public class WelcomePage {
             Button entryButton = factory.buttonCreation(primaryStage ,null,"welcome","Enter");
 
             //Image backgroundImage = new Image("C:\\Users\\Léonard\\Documents\\GitHub\\java_rocketLeague_FX\\src\\images\\fondRocketLeague.jpg");
-            //Image backgroundImage = new Image("C:\\Users\\Robin\\OneDrive\\Documents\\GitHub\\java_rocketLeague_FX\\src\\images\\fondRocketLeague.jpg");
-            Image backgroundImage = new Image("C:\\Users\\merlin\\Desktop\\iesn\\bloc 2\\java\\ProjetJavaFxV0\\ProjetJavaFxV0\\src\\images\\fondRocketLeague.jpg");
+            Image backgroundImage = new Image("C:\\Users\\Robin\\OneDrive\\Documents\\GitHub\\java_rocketLeague_FX\\src\\images\\fondRocketLeague.jpg");
+            //Image backgroundImage = new Image("C:\\Users\\merlin\\Desktop\\iesn\\bloc 2\\java\\ProjetJavaFxV0\\ProjetJavaFxV0\\src\\images\\fondRocketLeague.jpg");
+
             BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
             BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
 
